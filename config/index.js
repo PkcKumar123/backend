46ea3712fd895b8f06f8e3227fc60081e4def619
Prashant <prashantchoubey8338@gmail.com>
2022-07-14 20:34:11 +0530

    first commit

--- config/index.js
+++ config/index.js
@@ -1,11 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const {
-  APP_PORT,
-  DEBUG_MODE,
-  DB_URL,
-  JWT_SECRET,
-  REFRESH_SECRET,
-  APP_URL,
-} = process.env;
+export const { PORT, DEBUG_MODE, DB_URL, JWT_SECRET, REFRESH_SECRET, APP_URL } =
+  process.env;
